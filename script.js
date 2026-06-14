5df0a7e8e7163c73fab7ea62057cf38873d6b95c
Priyanshi Yadav <priyanshiy1312@gmail.com>
2026-06-14 17:02:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,76 @@ const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
 const userScorePara = document.querySelector("#user-score");
 const compuScorePara = document.querySelector("#compu-score");
+function showEmojiRainUser() {
+    const emoji = "👍";
+
+    for (let i = 0; i < 20; i++) {
+        const span = document.createElement("span");
+
+        span.innerText = emoji;
+        span.style.position = "fixed";
+        span.style.left = Math.random() * window.innerWidth + "px";
+        span.style.top = "-50px";
+        span.style.fontSize = Math.random() * 30 + 20 + "px";
+        span.style.zIndex = "9999";
+        span.style.pointerEvents = "none";
+
+        document.body.appendChild(span);
+
+        const duration = Math.random() * 3000 + 2000;
+
+        span.animate(
+            [
+                { transform: "translateY(0px)" },
+                { transform: `translateY(${window.innerHeight + 100}px)` }
+            ],
+            {
+                duration: duration,
+                iterations: 1,
+                easing: "linear"
+            }
+        );
+
+        setTimeout(() => {
+            span.remove();
+        }, duration);
+    }
+}
+function showEmojiRainCompu() {
+    const emoji = "👎";
+
+    for (let i = 0; i < 20; i++) {
+        const span = document.createElement("span");
+
+        span.innerText = emoji;
+        span.style.position = "fixed";
+        span.style.left = Math.random() * window.innerWidth + "px";
+        span.style.top = "-50px";
+        span.style.fontSize = Math.random() * 30 + 20 + "px";
+        span.style.zIndex = "9999";
+        span.style.pointerEvents = "none";
+
+        document.body.appendChild(span);
+
+        const duration = Math.random() * 3000 + 2000;
+
+        span.animate(
+            [
+                { transform: "translateY(0px)" },
+                { transform: `translateY(${window.innerHeight + 100}px)` }
+            ],
+            {
+                duration: duration,
+                iterations: 1,
+                easing: "linear"
+            }
+        );
+
+        setTimeout(() => {
+            span.remove();
+        }, duration);
+    }
+}
 const drawGame = () =>{
     
      msg.innerText ="game was draw";
@@ -13,6 +83,7 @@ const showWinner =(userWin,userChoice,compuChoice) =>{
     if(userWin){
          userScore++;
        userScorePara.innerText = userScore;
+        showEmojiRainUser();
         
         msg.innerText =`You win! .your ${userChoice} beats ${compuChoice}`;
         msg.style.backgroundColor ="green";
@@ -20,6 +91,7 @@ const showWinner =(userWin,userChoice,compuChoice) =>{
     else{
          compuScore++;
        compuScorePara.innerText = compuScore;
+        showEmojiRaincompu();
         
          msg.innerText =`You lost. your ${userChoice} beats ${compuChoice}`;
           msg.style.backgroundColor ="red";
